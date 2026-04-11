df59e44c3258f24fd7b4f5476b41fc69c650ac44
Revathireddy08 <revathireddy355@gmail.com>
2026-04-11 16:28:14 +0530

    final

--- api/index.js
+++ api/index.js
@@ -38,20 +38,16 @@ app.use(morgan("dev"));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true,
   })
 );
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
