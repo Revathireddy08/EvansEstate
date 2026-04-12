7c9f4a845ecb1c136b871ebe9ea9b714d76174e5
Revathireddy08 <revathireddy355@gmail.com>
2026-04-12 13:04:55 +0530

    last update

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteteUser, testing, updateUser, getUserListing, getUser } from '../controllers/user.controller.js';
+import { deleteUser, testing, updateUser, getUserListing, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
